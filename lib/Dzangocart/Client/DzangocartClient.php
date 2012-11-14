e05db60e4b94cf6db632478013ea71eefd3b725a
Alexander Kozlov <alexander.kozlov@simbirsoft.com>
2012-11-14 16:49:17 +0400

    added encrypt and encode methods

--- lib/Dzangocart/Client/DzangocartClient.php
+++ lib/Dzangocart/Client/DzangocartClient.php
@@ -2,6 +2,29 @@
 
 namespace Dzangocart\Client;
 
-class DzangocartClient {
-
+class DzangocartClient 
+{
+    public static function encode($data, $key, $expires) 
+    {
+        if (!array_key_exists('expires', $data)) {
+            $data['expires'] = date('c', time() + $expires);
+        }
+        
+        $userdata = json_encode($data);
+        
+        return self::encrypt($userdata, $key);
+    }
+  
+    public static function encrypt($data, $key) 
+    {
+        try {
+            $cipher = new \pcrypt($key);
+            $result = $cipher->cipher($data);
+        }
+        catch (ErrorException $e) {
+            $result = null;
+        }
+        
+        return $result;
+    }
 }
