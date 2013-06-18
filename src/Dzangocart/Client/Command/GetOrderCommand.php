2109a4b8f4c4e279552a1ea17ee82a65f9f78088
Olivier Pichon <op@united-asian.com>
2013-06-18 14:54:45 +0700

    implemented order command

---
+++ src/Dzangocart/Client/Command/GetOrderCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Dzangocart\Client\Command;
+
+use Dzangocart\OM\Order;
+
+class GetOrderCommand extends AbstractCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function build()
+    {
+        parent::build();
+        $this->set('command.response_processing', 'raw');
+    }
+
+    public function process()
+    {
+        parent::process();
+
+//        $order = new Order($this['result']);
+
+//        $this->result = $order;
+    }
+}
