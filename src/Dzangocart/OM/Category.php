a3e867f314a773ccb62f70169f04a4540e5cff4f
Pratik Raj Sharma <pratik@crossovernepal.com>
2014-07-03 13:43:01 +0545

    mapped each array members

--- src/Dzangocart/OM/Category.php
+++ src/Dzangocart/OM/Category.php
@@ -4,6 +4,26 @@ namespace Dzangocart\OM;
 
 class Category extends DzangocartObject
 {
+    public function getName()
+    {
+        return $this->data['name'];
+    }
+
+    public function getCode()
+    {
+        return $this->data['code'];
+    }
+
+    public function getSuffix()
+    {
+        return $this->data['suffix'];
+    }
+
+    public function getPrice()
+    {
+        return $this->data['price'];
+    }
+
     public function getTaxIncluded()
     {
         return (bool)$this->data['taxIncluded'];
@@ -27,5 +47,15 @@ class Category extends DzangocartObject
     public function getPack()
     {
         return (bool)$this->data['pack'];
+    }
+
+    public function getMinQuantity()
+    {
+        return $this->data['minQuantity'];
+    }
+
+    public function getMaxQuantity()
+    {
+        return $this->data['maxQuantity'];
     }
 }
