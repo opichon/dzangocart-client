dac559512b992798144cbcc3b8e65acc931c1e8d
Pratik Raj Sharma <pratik@crossovernepal.com>
2014-07-04 18:13:42 +0545

    updated class to use set method

--- src/Dzangocart/OM/Category.php
+++ src/Dzangocart/OM/Category.php
@@ -9,9 +9,19 @@ class Category extends DzangocartObject
         return $this->data['name'];
     }
 
+    public function setName($name)
+    {
+        $this->data['name'] = $name ;
+    }
+
     public function getCode()
     {
         return $this->data['code'];
+    }
+
+    public function setCode($code)
+    {
+        $this->data['code'] = $code ;
     }
 
     public function getSuffix()
@@ -19,9 +29,19 @@ class Category extends DzangocartObject
         return $this->data['suffix'];
     }
 
+    public function setSuffix($suffix)
+    {
+        $this->data['suffix'] = $suffix ;
+    }
+
     public function getPrice()
     {
         return $this->data['price'];
+    }
+
+    public function setPrice($price)
+    {
+        $this->data['price'] = $price ;
     }
 
     public function getTaxIncluded()
@@ -29,9 +49,19 @@ class Category extends DzangocartObject
         return (bool)$this->data['taxIncluded'];
     }
 
+    public function setTaxIncluded($taxIncluded)
+    {
+        (bool)$this->data['taxIncluded'] = $taxIncluded ;
+    }
+
     public function getExport()
     {
         return (bool)$this->data['export'];
+    }
+
+    public function setExport($export)
+    {
+        (bool)$this->data['export'] = $export;
     }
 
     public function getShipping()
@@ -39,9 +69,19 @@ class Category extends DzangocartObject
         return (bool)$this->data['shipping'];
     }
 
+    public function setShipping($shipping)
+    {
+        (bool)$this->data['shipping'] = $shipping;
+    }
+
     public function getDownload()
     {
         return (bool)$this->data['download'];
+    }
+
+    public function setDownload($download)
+    {
+       (bool)$this->data['download'] = $download;
     }
 
     public function getPack()
@@ -49,13 +89,28 @@ class Category extends DzangocartObject
         return (bool)$this->data['pack'];
     }
 
+    public function setPack($pack)
+    {
+        (bool)$this->data['pack'] = $pack;
+    }
+
     public function getMinQuantity()
     {
         return $this->data['minQuantity'];
     }
 
+    public function setMinQuantity($minQuantity)
+    {
+        $this->data['minQuantity'] = $minQuantity;
+    }
+
     public function getMaxQuantity()
     {
         return $this->data['maxQuantity'];
+    }
+
+    public function setMaxQuantity($maxQuantity)
+    {
+        $this->data['maxQuantity'] = $maxQuantity ;
     }
 }
